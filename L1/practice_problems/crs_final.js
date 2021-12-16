3f329d1a6980977b6a20b6a329efdb2c387f7bee
amandeep1420 <amandeep1420@gmail.com>
2021-12-16 00:23:38 +0000

    Finished CRS exercise.

--- L1/practice_problems/crs_final.js
+++ L1/practice_problems/crs_final.js
@@ -1,29 +1,34 @@
+"use strict";
+
 function generateClassRecordSummary(scores) {
   let allGrades      = [];
   let examsArrays    = [];
   let studentObjects = Object.values(scores);
-  
+
   studentObjects.forEach(student => {
     let examScores     = student.scores.exams;
     let exerciseScores = student.scores.exercises;
-    
+
     examsArrays.push(examScores);
-    
+
     let numericGrade = findNumericGrade(examScores, exerciseScores);
     let letterGrade  = findLetterGrade(numericGrade);
     allGrades.push(`${numericGrade} (${letterGrade})`);
   });
-  
+
   let examsInfoArray = generateExamsInfo(examsArrays);
-  
+
   return {studentGrades: allGrades, exams: examsInfoArray};
 }
 
 function findNumericGrade(exams, exercises) {
-  let averageExamScore   = exams.reduce((total, score) => total + score) / 4;
-  let totalExerciseScore = exercises.reduce((total, score) => total + score);
-  
-  let finalGrade = (averageExamScore * 0.65) + (totalExerciseScore * 0.35);
+  const EXAM_WEIGHT     = 0.65;
+  const EXERCISE_WEIGHT = 0.35;
+
+  let examsAvg     = exams.reduce((sum, score) => sum + score) / exams.length;
+  let exerciseAvg  = exercises.reduce((total, score) => total + score);
+
+  let finalGrade = (examsAvg * EXAM_WEIGHT) + (exerciseAvg * EXERCISE_WEIGHT);
   return Math.round(finalGrade);
 }
 
@@ -46,19 +51,19 @@ function findLetterGrade(numericGrade) {
 function generateExamsInfo(exams) {
   let scoreArraysForEachExam = [];
   let numberOfExams          = exams[0].length;
-  
+
   for (let examIndex = 0; examIndex < numberOfExams; examIndex += 1) {
     let scoresForOneExam = [];
     exams.forEach(examArr => scoresForOneExam.push(examArr[examIndex]));
     scoreArraysForEachExam.push(scoresForOneExam);
   }
-  
+
   return scoreArraysForEachExam.map(scores => {
     let avgScore = scores.reduce((total, num) => total + num) / scores.length;
     avgScore     = parseFloat(avgScore.toFixed(1), 10);
-    let minScore = scores.reduce((min, num) => min < num ? min : num);
-    let maxScore = scores.reduce((max, num) => max < num ? num : max);
-    
+    let minScore = scores.reduce((min, num) => (min < num ? min : num));
+    let maxScore = scores.reduce((max, num) => (max < num ? num : max));
+
     return ({average: avgScore, minimum: minScore, maximum: maxScore});
   });
 }
